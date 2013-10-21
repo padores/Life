98965d73a902799b2acf27904489a045ce8b3caa
Gabriel Pedrosa <pedrosa@outlook.com>
2013-10-20 23:28:50 -0200

    Arrumando a quebra de linha entre as interações do jogo

--- Life.java
+++ Life.java
@@ -24,53 +24,47 @@ public class Life {
 				} else {
 					System.out.printf(".");
 				}
-				System.out.println();
 			}
 			System.out.println();
 		}
+		System.out.println();
 	}
-		
-		int vizinhos(int i, int j){
-			return matriz[i-1][j-1] + matriz[i-1][j] + matriz[i-1][j+1] + matriz[i][j-1] + matriz[i][j+1] + matriz[i+1][j-1] + matriz[i+1][j] + matriz[i+1][j+1];
-		}
-		
-		int [][] interacao(){
-			int [][] aux = new int [MAX][MAX];
-			int i, j;
-			
-			for(i =1; i<MAX-1; i++){
-				for(j = 1; j<MAX -1; j++){
-					if(matriz[i][j] == 1){
-						if((vizinhos(i, j)< 2) || (vizinhos(i, j) > 3)){
-							aux[i][j] = 0;
-						}
-						else{
-							aux[i][j] = 1;
-						}
+
+	int vizinhos(int i, int j) {
+		return matriz[i - 1][j - 1] + matriz[i - 1][j] + matriz[i - 1][j + 1]
+				+ matriz[i][j - 1] + matriz[i][j + 1] + matriz[i + 1][j - 1]
+				+ matriz[i + 1][j] + matriz[i + 1][j + 1];
+	}
+
+	int[][] interacao() {
+		int[][] aux = new int[MAX][MAX];
+		int i, j;
+
+		for (i = 1; i < MAX - 1; i++) {
+			for (j = 1; j < MAX - 1; j++) {
+				if (matriz[i][j] == 1) {
+					if ((vizinhos(i, j) < 2) || (vizinhos(i, j) > 3)) {
+						aux[i][j] = 0;
+					} else {
+						aux[i][j] = 1;
 					}
-						else{
-							if(vizinhos(i, j) == 3){
-								aux[i][j] = 1;
-							}
-							else{
-								aux[i][j] = 0;
-							}
-						}
+				} else {
+					if (vizinhos(i, j) == 3) {
+						aux[i][j] = 1;
+					} else {
+						aux[i][j] = 0;
 					}
 				}
-			return aux;
-			}
-	
-			
-	
-
-		
-		void simulaVida(int quant){
-			int i;
-			for(i = 0; i < quant; i++){
-				imprimeTabuleiro();
-				matriz = interacao();
 			}
 		}
+		return aux;
 	}
 
+	void simulaVida(int quant) {
+		int i;
+		for (i = 0; i < quant; i++) {
+			imprimeTabuleiro();
+			matriz = interacao();
+		}
+	}
+}
